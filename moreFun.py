de220d77f6e5e6a1f8f00361dc6a3f36dc272ec4
BranJ7 <bjorda73@gmail.com>
2026-02-19 02:09:59 +0000

    learning as we go!

---
+++ moreFun.py
@@ -0,0 +1,39 @@
+print("Hello")
+print("My")
+print("Students")
+
+print("Hello", "My", "Students")
+
+print("Hello" + "My" + "Students")
+
+name = "Bran"
+
+print("My friend is", name)
+
+#print(f"My friend is": {name}")
+
+user_name = input("What is your name?")
+print(f"Your name is: {user_name}")
+
+num_1 = input("give me your ")
+num_2 = input("give me another number")
+total = int(num_1) + int(num_2)
+
+# ''' ''' multi line comment
+# # single line comment
+
+name = "bran"
+age = "299"
+city = "Kansas City"
+
+def my_fun_1(city):
+    name = "reese"
+    age = "21"
+    print(name, age, city)
+
+    print(name, age, city)
+
+    print(city)
+    my_fun_1(city)
+
+
