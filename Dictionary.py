bef44396ab11238eefdb54529e19cd21e2d2363d
BranJ7 <bjorda73@gmail.com>
2026-02-20 02:46:49 +0000

    playing around, learning.

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+teacher = {"name":"mike", "age":"48", "city":"boston"}
+print(teacher["name"], "is", teacher["age"], "he stays in", teacher["city"])
+
+him = {"favoriteColor":"green", "favoriteNumber":"seven", "favoriteHobbie":"drawing"}
+
+print("That is Brandon and his favorite color is", him["favoriteColor"],
+       "and he love the number", him["favoriteNumber"],
+         "and he also loves to", him["favoriteHobbie"])
